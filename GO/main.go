a8ea4be888e87091863544d2b92fd0f3cafab543
IdekDude <74634965+IdekDude@users.noreply.github.com>
2022-04-20 11:23:41 -0500

    Removed outer for loop in golang pow

--- GO/main.go
+++ GO/main.go
@@ -18,23 +18,23 @@ func getHash(input string, paddingLength int) string {
 	var list []HashedSolution
 	padding := strings.Repeat("0", paddingLength)
 
-	for i := 0; i < 10; i++ {
-		for postFix := 0; ; postFix++ {
-			str := input + strconv.Itoa(postFix)
-			hash := sha256.New()
-			hash.Write([]byte(str))
-			encodedHash := hex.EncodeToString(hash.Sum(nil))
+	for postFix := 0; ; postFix++ {
+		str := input + strconv.Itoa(postFix)
+		hash := sha256.New()
+		hash.Write([]byte(str))
+		encodedHash := hex.EncodeToString(hash.Sum(nil))
 
-			if strings.HasPrefix(encodedHash, padding) {
+		if strings.HasPrefix(encodedHash, padding) {
+			for i := 0; i < 10; i++ {
 				list = append(list, HashedSolution{
 					Postfix: postFix,
 					Hash:    encodedHash,
 				})
-				break
 			}
+
+			jsonBytes, _ := json.Marshal(list)
+
+			return b64.StdEncoding.EncodeToString(jsonBytes)
 		}
 	}
-	jsonBytes, _ := json.Marshal(list)
-
-	return b64.StdEncoding.EncodeToString(jsonBytes)
 }

ea172c776207452af4a40e1c27e2143c96d374e4
IdekDude <74634965+IdekDude@users.noreply.github.com>
2022-04-20 11:11:06 -0500

    Removed outer for loop in JS pow

--- JS/main.js
+++ JS/main.js
@@ -2,20 +2,15 @@ const crypto = require('crypto')
 
 
 function getHash(input, paddingLength) {
-    let final = [];
     const padding = "0".repeat(paddingLength);
-    for (let i = 0; i < 10; i ++) {
-        for (let postFix = 0; true; postFix++) {
-            const target = input + postFix.toString(),
-                encodedHash = crypto.createHash("sha256").update(target).digest("hex");
-            if (encodedHash.indexOf(padding) === 0) {
-                final.push({
-                    postfix: postFix,
-                    hash: encodedHash,
-                });
-                break;
-            }
+    for(let postFix = 0; true; postFix++) {
+        const target = input + postFix.toString(),
+            encodedHash = crypto.createHash("sha256").update(target).digest("hex");
+        if(encodedHash.indexOf(padding) === 0) {
+            return btoa(JSON.stringify(Array(10).fill({
+                postfix: postFix,
+                hash: encodedHash,
+            })))
         }
     }
-    return btoa(JSON.stringify(final));
 }

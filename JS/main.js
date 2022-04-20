eee943d253c7aaf8444cc4cc8e8735793f317b3e
IdekDude <74634965+IdekDude@users.noreply.github.com>
2022-04-19 19:22:44 -0500

    Added JS pow

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto')
+
+
+function getHash(input, paddingLength) {
+    let final = [];
+    const padding = "0".repeat(paddingLength);
+    for (let i = 0; i < 10; i ++) {
+        for (let postFix = 0; true; postFix++) {
+            const target = input + postFix.toString(),
+                encodedHash = crypto.createHash("sha256").update(target).digest("hex");
+            if (encodedHash.indexOf(padding) === 0) {
+                final.push({
+                    postfix: postFix,
+                    hash: encodedHash,
+                });
+                break;
+            }
+        }
+    }
+    return btoa(JSON.stringify(final));
+}

23d8b3916f8ad5691c2469640fb5838ea2741c4d
IdekDude <74634965+IdekDude@users.noreply.github.com>
2022-07-10 20:09:16 -0500

    Updated TS

--- TS/main.ts
+++ TS/main.ts
@@ -1,21 +1,22 @@
 import * as crypto from 'crypto'
 
 
-function getHash(input: string, paddingLength: number) {
+function getHash(input: string, paddingLength: number, runs: number) {
     let final: any[] = [];
     const padding = "0".repeat(paddingLength);
-    for (let i = 0; i < 10; i ++) {
-        for (let postFix = 0; true; postFix++) {
-            const target = input + postFix.toString(),
-                encodedHash = crypto.createHash("sha256").update(target).digest("hex");
-            if (encodedHash.indexOf(padding) === 0) {
-                final.push({
-                    postfix: postFix,
-                    hash: encodedHash,
-                });
-                break;
-            }
+    let currentRuns = 0;
+    for (let postFix = 0; true; postFix++) {
+        const target = input + postFix.toString(),
+            encodedHash = crypto.createHash("sha256").update(target).digest("hex");
+        if (encodedHash.indexOf(padding) === 0) {
+            final.push({
+                postfix: postFix,
+                hash: encodedHash,
+            });
+            currentRuns++;
+        }
+        if (currentRuns === runs) {
+            return final;
         }
     }
-    return btoa(JSON.stringify(final));
 }
